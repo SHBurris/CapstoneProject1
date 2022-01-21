a9279b4df1e3ab46b63df0972e26ae0f4e04b82f
Za-Sing <za.sing227@gmail.com>
2022-01-21 01:36:53 +0000

    Maven with JUnit setup

---
+++ src/test/java/cs4263/oucapstone/app/AppTest.java
@@ -0,0 +1,20 @@
+package cs4263.oucapstone.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
