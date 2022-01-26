20fc28b063fe41ed07674c40f23faa2e9b13a3c3
thetateman <47228223+thetateman@users.noreply.github.com>
2022-01-25 20:47:02 -0600

    Update ParserTest.java

--- src/test/java/cs4263/oucapstone/app/ParserTest.java
+++ src/test/java/cs4263/oucapstone/app/ParserTest.java
@@ -1,6 +1,6 @@
 package cs4263.oucapstone.app;
 
-import static org.junit.jupiter.api.assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
