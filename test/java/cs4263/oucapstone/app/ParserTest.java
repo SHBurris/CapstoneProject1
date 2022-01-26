834a0f910191716f19a3e84dd38bedde170e36e2
thetateman <tdsmith2018@gmail.com>
2022-01-25 20:29:47 -0600

    changed directory structure

---
+++ test/java/cs4263/oucapstone/app/ParserTest.java
@@ -0,0 +1,20 @@
+package cs4263.oucapstone.app;
+
+import static org.junit.jupiter.api.assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParserTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
